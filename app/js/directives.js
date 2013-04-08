0c64367a9f075b42ab8573a2a42601a40cfa3805
Leo Shmuylovich <leo@noredink.com>
2013-04-08 09:42:12 -0400

    add tracking of css top and left to scope. This is hopefully a more stable solution for left and top resizing

--- app/js/directives.js
+++ app/js/directives.js
@@ -14,21 +14,18 @@ angular.module('myApp.directives', []).
   directive('resizeable', function($compile){
     var resize = function (scope, elm, ev){
           if(scope.y_resizing){
-
             var min_height = 0;
             var delta_height = (scope.resize_top ? -1 : 1) * (ev.pageY-scope.start_position.ypos);
             var new_height = scope.start_size.height + delta_height;
-
             var constraints_met = false;
             if(new_height > min_height){
               constraints_met =  true
-
             }
             if (constraints_met){
-
               if(scope.resize_top){
-                var new_top = ev.pageY-scope.parent_start_position.top
-                elm.css('top', new_top +'px')
+                var delta_top = ev.pageY-scope.start_position.ypos;
+                var new_top = delta_top + parseInt(scope.start_top.split('px')[0]);
+                elm.css('top', new_top);
               }
               elm.height(new_height);
             }
@@ -46,7 +43,9 @@ angular.module('myApp.directives', []).
             }
             if (constraints_met){
               if(scope.resize_left){
-                var new_left = ev.pageX-scope.parent_start_position.left;
+                // left is defined relative to parent left
+                var delta_left = ev.pageX-scope.start_position.xpos;
+                var new_left = delta_left + parseInt(scope.start_left.split('px')[0]);
                 elm.css('left', new_left +'px');
               }
               elm.width(new_width);
@@ -77,7 +76,8 @@ angular.module('myApp.directives', []).
             scope.parent_start_position = elm.position();
             scope.start_position = {xpos: ev.pageX, ypos:ev.pageY};
             scope.start_size = {width: elm.width(), height: elm.height()};
-
+            scope.start_left = (elm.css('left') == "auto") ? '0px' : elm.css('left')
+            scope.start_top = (elm.css('top') == "auto") ? '0px' : elm.css('top')
             // tell the directive that we are actively resizing
 
 
@@ -94,6 +94,8 @@ angular.module('myApp.directives', []).
             // scope.start_position = {xpos: ev.pageX, ypos:ev.pageY};
             // console.log('new_start', scope.start_position);
             scope.start_size = {width: elm.width(), height: elm.height()};
+            scope.start_left = (elm.css('left') == "auto") ? '0px' : elm.css('left')
+            scope.start_top = (elm.css('top') == "auto") ? '0px' : elm.css('top')
             scope.x_resizing = false;
             scope.y_resizing = false;
           });
